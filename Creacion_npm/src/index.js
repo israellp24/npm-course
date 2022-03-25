8d5cba2e719d0d921138d24cf5844c94dd31512c
israellp24 <41647280+israellp24@users.noreply.github.com>
2022-03-24 23:08:08 -0600

    fin curso

---
+++ Creacion_npm/src/index.js
@@ -0,0 +1,19 @@
+// Se declara el arreglo
+const messages = [
+    "David",
+    "Diana",
+    "Ana Maria",
+    "Isabela",
+    "Antonio",
+    "Norma"
+]
+
+//Crear función para enviar aleatoriamente  los nombres del arreglo
+const randomMsg = () => {
+const message = messages[Math.floor(Math.random()*messages.length)]
+    console.log(message)
+}
+
+// Exportar como un módulo
+
+module.exports = { randomMsg }
